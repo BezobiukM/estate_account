e3bdc63429498c186ca71199f2b40e8d11536f94
BezobiukM <bezobiuk.mari@gmail.com>
2023-01-22 22:58:39 +0200

    Added invoice creation on button Sold click.

--- models/estate_property.py
+++ models/estate_property.py
@@ -9,11 +9,15 @@ class EstateProperty(models.Model):
     _inherit = 'estate.property'
 
     def action_property_sold(self):
+        accountMove = self.env['account.move'].sudo()
         for record in self:
             _logger.error("$$$$$action_property_sold$$$$$$$$$$$\n")
             _logger.error(record.state)
             _logger.error("$$$$$action_property_sold_end$$$$$$$$$$$")
+
+            accountMove.create({
+                'partner_id': record.salesman,
+                'move_type': 'out_invoice',
+                'invoice_origin': record.name,
+            })
         return super(EstateProperty, self).action_property_sold()
- 
-    
-    
